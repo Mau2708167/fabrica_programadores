b16b520fe4078bc7097d41cae7bea0436f3def41
Mau2708167 <maumeloaraujo@gmail.com>
2026-05-09 11:50:13 -0300

    Add files via upload

---
+++ 03_python_entradas/entradas6.py
@@ -0,0 +1,13 @@
+#Autor:Mauricio Melo
+
+#variaveis
+nome=input('me diga seu nome')
+peso=float(input('me diga seu peso:'))
+altura=float(input('me diga sua altura:'))
+imc=peso/(altura*altura)
+
+#exibindo resultado
+print(f'O seu imc é:{imc:.2f}')
+
+
+
