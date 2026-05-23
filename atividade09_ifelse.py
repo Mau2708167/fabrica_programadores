c66bdc2dca71627c914fcd1babeb0434153df759
Mau2708167 <maumeloaraujo@gmail.com>
2026-05-23 08:54:15 -0300

    Desvios condicionais

---
+++ atividade09_ifelse.py
@@ -0,0 +1,9 @@
+temperatura= float(input("digite a temeparatura em celsius:"))
+if temperatura>=30:
+    print("está quente!")
+elif temperatura>=20:
+    print("esta agradavel.")
+elif temperatura>=10:
+    print("esta frio!")
+else:
+    print("esta muito frio!")
